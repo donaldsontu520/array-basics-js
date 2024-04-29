bb6fe82a9cebd8705705d45cacfb82f55846fde0
donaldsontu520 <153301884+donaldsontu520@users.noreply.github.com>
2024-04-29 11:06:29 -0400

    updated html

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 
-// 29 April 2024
-// Project
-
 
  //Input 
+ const firstName = "Tucker";
+ const lastName = "Donaldson";
+ const date = "29 April 2024";
+ const title ="Array basics";
 
+ const fruits = ["Banana", "Orange", "Apple", "Mango"];
+ fruits.pop();
 
+ const countries = ["MERICA", "stailia", "CHINA", "KOREAaAa"]
+ countries.push();
   //process 
-  const fruits = ["Banana", "Orange", "Apple", "Mango"];
-	fruits.pop();
+ 
 
 
 
@@ -16,8 +20,14 @@
 
   //Output 
 
-  
-
+  const myOutput =`
+  ${firstName} ${lastName}
+  ${date}
+  ${title}
+  ${fruits}
+  ${countries}
+  `
+ console.log(myOutput)
 
 
 
